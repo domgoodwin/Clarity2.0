53cbe2db5989c81c599f5b9d080adc187404e9c3
Minshall, Olivia <olivia.minshall@capgemini.com>
2017-05-03 11:35:09 +0100

    Added in functions to session.php from login.php

--- Login.php
+++ Login.php
@@ -6,77 +6,18 @@
 <h1>Timebooking Login</h1>
 
 <?php
-// Start the session
-session_start();
+include 'Session.php';
 
-// Set session variables
-$_SESSION["username"] = "";
-$_SESSION["password"] = "";
-$_SESSION["access"] = "";
 
-// define variables and set to empty valuess
+// define variables and set to empty values
+	$pw = "";
 	$usernameErr = $passwordErr = "";
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "clarity2";
-	$tbname = "users";
-	
-  function SendSQL($sql){
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "clarity2";
+	$userID = "";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $result = $conn->query($sql);
-    return $result;
-    $conn->close();
-  }
-  
-  function ValidateUser($UN, $PW){
-    $sql = "SELECT User_ID, Username, Password, Role_ID FROM users WHERE Username=\"" . $UN ."\"" and "Password=\"" . $PW ."\"";
-    $output = "<table><tr><th>Username</th><th>Password</th></tr>";
-    $result = SendSQL($sql);
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            $output .= "<tr>" . "<td>" . $row["Username"] . "</td>" . "<td>" . $row["Password"] . "</td>" . "</tr>";
-        }
-    } else {
-        $output .= "<tr>No results</tr>";
-    }
-    $output .= "</table>";
-    echo $output;
-  }
-  
-    function ValidateSession($UN){
-	  $sql = "SELECT User_ID, Username, Password, Role_ID FROM users WHERE Username=\"" . $UN ."\"";
-	  $output = "<table><tr><th>Username</th><th>Role_ID</th></tr>";
-	  $result = SendSQL($sql);
-	  $row = $result->fetch_assoc();
-	  if ($row["Role_ID"] == 1){
-		$output .= "<tr>Standard user</tr>";
-		$_SESSION["access"] = Standard;
-    } else {
-		if ($row["Role_ID"] == 2){
-        $output .= "<tr>Admin user</tr>";
-		$_SESSION["access"] = Admin;
-		}
-    }
-    $output .= "</table>";
-    echo $output;
-  }
   
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $_SESSION["username"] = test_input($_POST["username"]);
-  $_SESSION["password"] = test_input($_POST["password"]); 
+  $pw = test_input($_POST["password"]); 
 }
 
 function test_input($data) {
@@ -85,25 +26,22 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
+Login($_SESSION["username"],$pw);
+GetRole($_SESSION["username"]);
+GetLogin($_SESSION["login"]);
+
+if(array_key_exists('logout', $_POST)){
+	Logout();
+}
 ?>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><br>
 Username: <input type="text" name="username" required> <span class="error">*</span><br><br>
 Password: <input type="password" name="password" required> <span class="error">*</span><br>
-<input type="submit">
+<input type="submit" name="login" id="login" value="Login">
 </form>
-<?php
-
-
-ValidateUser($_SESSION["username"],$_SESSION["password"]);
-ValidateSession($_SESSION["username"]);
-
-echo "<h2>Input:</h2>";
-echo $_SESSION["username"];
-echo "<br>";
-echo $_SESSION["password"];
-echo "<br>";
-echo $_SESSION["access"];
-
-?>
+<form method="POST" name="logout" action"/Login.php">
+<input type="submit" name="logout" id="logout" value="Logout">
+</form>
 </body>
 </html>
---
+++ Session.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+
+include "Database_Functions.php";
+
+function Login($UN, $PW){
+    $sql = "SELECT User_ID, Username, Password, Role_ID FROM users WHERE Username=\"" . $UN ."\"" and "Password=\"" . $PW ."\"";
+    $result = SendSQL($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+			$_SESSION["login"] = true;
+        }
+    } else {
+        $output .= "<tr>No results</tr>";
+		$_SESSION["login"] = false;
+    }
+  }
+  
+function GetRole($UN){
+	$sql = "SELECT User_ID, Username, Password, Role_ID FROM users WHERE Username=\"" . $UN ."\"";
+	$result = SendSQL($sql);
+	$row = $result->fetch_assoc();
+	if ($row["Role_ID"] == 1){
+		$output .= "<tr>Standard user</tr>";
+		$_SESSION["role"] = Standard;
+    } elseif ($row["Role_ID"] == 2){
+        $output .= "<tr>Admin user</tr>";
+		$_SESSION["role"] = Admin;
+    }
+}
+ // Get user ID - returns USER ID if logged in - else 0 (not logged in)
+function GetLogin($login){
+	if (isset($login) && $login == true) {
+		$sql = "SELECT User_ID FROM users WHERE Username=\"" . $_SESSION['username'] ."\"";
+		$result = SendSQL($sql);
+		while($row = $result->fetch_assoc()) {
+			echo "Welcome to the member's area, " . $_SESSION['username'] . "!" ;
+			$userID = $row[User_ID];
+		}
+		} else {
+    echo "Please log in first to see this page.";
+	$userID = 0;
+	}
+}
+function Logout(){
+	$_SESSION["username"] = "";
+	$_SESSION["role"] = "";
+	$_SESSION["login"] = "";
+}
+?>
