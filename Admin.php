80b1b4d7e45027e89cac6b7c45e22d15ae847591
Minshall, Olivia <olivia.minshall@capgemini.com>
2017-05-08 15:37:30 +0100

    formatting admin page

--- Admin.php
+++ Admin.php
@@ -1,34 +1,69 @@
 <html>
 <head>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 <title>Admin Panel</title>
 </head>
 
 <body>
-<?php
-include 'Database_Functions.php';
-// if(isset($_SESSION['access']!=Admin)){ //if login in session is not set
-//     header("Location: login.php");
-// }
-GetProjects();
-echo "<br>";
-GetUsers();
-echo "<br>";
-GetBookings();
+  <div class="row">
+		<div class="large-12 columns">
+			<div class="callout">
+				<div class="row">
+		      <div class="large-12 columns">
+		        <h1>Admin Control</h1>
+		      </div>
+		    </div>
+        <div class="row">
+					<div class="large-4 large-push-4 columns">
+					</div>
+				</div>
+      </div>
+    </div>
+  </div>
 
-if( isset($_POST['submitProject'])){
-  $Project_Ref = $_POST['Project_Ref'];
-  $Project_Name = $_POST['Project_Name'];
-  $Start_Date= $_POST['Start_Date'];
-  $End_Date = $_POST['End_Date'];
-  $result = CreateProject($Project_Ref, $Project_Name, $Start_Date, $End_Date);
-}
-if( isset($_POST['submitUser'])){
-  $Username = $_POST['Username'];
-  $Password = $_POST['Password'];
-  $Role = $_POST['Role'];
-  $result = CreateNewUser($Username, $Password, $Role);
-}
-?>
+  <div class="row">
+      <p><a data-open="exampleModal1">Click me for a modal</a></p>
+  </div>
+
+  <div class="reveal" id="exampleModal1" data-reveal>
+    <h1>Awesome. I Have It.</h1>
+    <?
+        echo 'Hello'; ?>
+    <p class="lead">Your couch. It is mine.</p>
+    <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+
+          <?php
+          include 'Database_Functions.php';
+          // if(isset($_SESSION['access']!=Admin)){ //if login in session is not set
+          //     header("Location: login.php");
+          // }
+          GetProjects();
+          // echo "<br>";
+          GetUsers();
+          // echo "<br>";
+          GetBookings();
+
+          if( isset($_POST['submitProject'])){
+            $Project_Ref = $_POST['Project_Ref'];
+            $Project_Name = $_POST['Project_Name'];
+            $Start_Date= $_POST['Start_Date'];
+            $End_Date = $_POST['End_Date'];
+            $result = CreateProject($Project_Ref, $Project_Name, $Start_Date, $End_Date);
+          }
+          if( isset($_POST['submitUser'])){
+            $Username = $_POST['Username'];
+            $Password = $_POST['Password'];
+            $Role = $_POST['Role'];
+            $result = CreateNewUser($Username, $Password, $Role);
+          }
+          ?>
+        </div>
 
 <?php if( isset($result) ) echo $result;?>
 
@@ -47,5 +82,10 @@ if( isset($_POST['submitUser'])){
   <input type="submit" name="submitUser" id="test" value="Create User"/>
 </form>
 
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
 </body>
 </html>
--- Database_Functions.php
+++ Database_Functions.php
@@ -25,48 +25,48 @@
   // Reads all projects from project
   function GetProjects($criteria){
     $sql = "SELECT Project_ID, Project_Ref, Project_Name FROM projects" . $criteria;
-    $output = "<table><tr><th>Project_Name</th><th>Project_Ref</th></tr>";
+    $output = "<div class='large-4 columns'><table class='table'><tr><th class='th'>Project_Name</th><th class='th'>Project_Ref</th></tr>";
     $result = SendSQL($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            $output .= "<tr>" . "<td>" . $row["Project_Ref"] . "</td>" . "<td>" . $row["Project_Name"] . "</td>" . "</tr>";
+            $output .= "<tr>" . "<td class='td'>" . $row["Project_Ref"] . "</td>" . "<td class='td'>" . $row["Project_Name"] . "</td>" . "</tr>";
         }
     } else {
         $output .= "<tr>No results</tr>";
     }
-    $output .= "</table>";
+    $output .= "</table></div>";
     echo $output;
   }
   function GetUsers($criteria){
-    $sql = "SELECT User_ID, Username, Password, Role_ID FROM users" . $criteria;
-    $output = "<table><tr><th>User ID</th><th>Username</th><th>Password</th><th>Role</th></tr>";
+    $sql = "SELECT User_ID, Username, Passwd, Role_ID FROM users" . $criteria;
+    $output = "<div class='large-4 columns'><table class='table'><tr><th class='th'>User ID</th><th class='th'>Username</th><th class='th'>Password</th><th class='th'>Role</th></tr>";
     $result = SendSQL($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            $output .= "<tr>" . "<td>" . $row["User_ID"] . "</td>" . "<td>" . $row["Username"] . "</td>" . "<td>" . $row["Password"] . "</td>" . "<td>" . $row["Role_ID"] . "</td>" . "</tr>";
+            $output .= "<tr>" . "<td class='td'>" . $row["User_ID"] . "</td>" . "<td class='td'>" . $row["Username"] . "</td>" . "<td class='td'>" . $row["Passwd"] . "</td>" . "<td class='td'>" . $row["Role_ID"] . "</td>" . "</tr>";
         }
     } else {
         $output .= "<tr>No results</tr>";
     }
-    $output .= "</table>";
+    $output .= "</table></div>";
     echo $output;
   }
   function GetBookings($criteria){
     $sql = "SELECT Booking_ID, Project_ID, User_ID, Date, Hours FROM bookings" . $criteria;
-    $output = "<table><tr><th>Booking ID</th><th>Project_ID</th><th>User_ID</th><th>Hours</th><th>Date</th></tr>";
+    $output = "<div class='large-4 columns'><table class='table'><tr><th class='th'>Booking ID</th><th class='th'>Project_ID</th><th class='th'>User_ID</th><th class='th'>Hours</th><th class='th'>Date</th></tr>";
     $result = SendSQL($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            $output .= "<tr>" . "<td>" . $row["Booking_ID"] . "</td>" . "<td>" . $row["Project_ID"] .
-             "</td>" . "<td>" . $row["User_ID"] . "</td>" . "<td>" . $row["Hours"] . "</td>" . "<td>" . $row["Date"] . "</td>"  . "</tr>";
+            $output .= "<tr>" . "<td class='td'>" . $row["Booking_ID"] . "</td>" . "<td class='td'>" . $row["Project_ID"] .
+             "</td>" . "<td class='td'>" . $row["User_ID"] . "</td>" . "<td class='td'>" . $row["Hours"] . "</td>" . "<td class='td'>" . $row["Date"] . "</td>"  . "</tr>";
         }
     } else {
         $output .= "<tr>No results</tr>";
     }
-    $output .= "</table>";
+    $output .= "</table></div>";
     echo $output;
   }
   function CreateProject($projRef, $projName, $startDate, $endDate){
@@ -79,7 +79,7 @@
     $sqlQuery = "SELECT * FROM users WHERE username = \"" . $username . "\"";
     $queryResult = SendSQL($sqlQuery);
     if ($queryResult->num_rows==0){
-      $sql  = "INSERT INTO users (Username, Password, Role_ID) VALUES (" . "\"" . $username . "\"" . "," . "\"" . $password . "\"" . "," . $roleID . ")";
+      $sql  = "INSERT INTO users (Username, Passwd, Role_ID) VALUES (" . "\"" . $username . "\"" . "," . "\"" . $password . "\"" . "," . $roleID . ")";
       echo "<br>" . $sql;
       $result = SendSQL($sql);
     } else {
@@ -128,7 +128,7 @@
     $sqlQuery = "SELECT User_ID FROM users WHERE username = \"" . $username . "\"";
     $queryResult = SendSQL($sqlQuery);
     if ($queryResult->num_rows==0){
-      $sql  = "INSERT INTO users (Username, Password, Role_ID, Passwd) VALUES (" . "\"" . $username . "\"" . "," . $roleID . ",\"" . $hashedPw . "\"" . ")";
+      $sql  = "INSERT INTO users (Username, Passwd, Role_ID, Passwd) VALUES (" . "\"" . $username . "\"" . "," . $roleID . ",\"" . $hashedPw . "\"" . ")";
       echo "<br>" . $sql;
       $result = SendSQL($sql);
     } else {
